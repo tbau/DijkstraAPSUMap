9a49068971c74762dda435b2e42478d871b2c44f
Thomas <tbau@my.apsu.edu>
2018-03-31 19:46:16 -0500

    Initial commit

---
+++ app/src/main/java/com/tbau/dijkstraapsumap/MainActivity.java
@@ -0,0 +1,65 @@
+package com.tbau.dijkstraapsumap;
+
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MapView mv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.start_button);
+        Button end = (Button) findViewById(R.id.end_button);
+        Button calc = (Button) findViewById(R.id.calc_button);
+
+        mv = (MapView) this.findViewById(R.id.mapView);
+
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.start_button){
+            mv.first = true;
+        }
+        else if(view.getId()==R.id.end_button){
+            mv.first = false;
+        }
+        else if(view.getId()==R.id.calc_button){
+            if(mv.selected[0]!=mv.selected[1]){
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(this);
+                }
+                builder.setTitle("Points")
+                        .setMessage("Your two points are ("
+                                +mv.clickablePoints.get(mv.selected[0]).x+", "
+                                +mv.clickablePoints.get(mv.selected[0]).y
+                                +mv.clickablePoints.get(mv.selected[1]).x+", "
+                                +mv.clickablePoints.get(mv.selected[1]).y)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // continue with delete
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/tbau/dijkstraapsumap/MapView.java
@@ -0,0 +1,115 @@
+package com.tbau.dijkstraapsumap;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thomas on 3/26/2018.
+ */
+
+public class MapView extends View implements View.OnTouchListener {
+
+    Bitmap MapBitmap;
+    int currentWidth;    //Width of View
+    int currentHeight;   //Height of View
+
+    Paint paint;         //Used to draw on Canvas
+
+    ArrayList<Point> clickablePoints;
+    int selected[]=new int[2];
+    boolean first=true;
+
+    public MapView(Context context) {
+        super(context);
+        setUp(null);
+    }
+
+    public MapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setUp(attrs);
+    }
+
+    public MapView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setUp(attrs);
+    }
+    public void setUp(AttributeSet attrs){
+
+        paint = new Paint();
+        paint.setColor(0xff000000);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+
+        MapBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.apsu_map); // Load bitmap for some allies
+        MapBitmap = Bitmap.createScaledBitmap(MapBitmap,1600,840,false);
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        //Get current width and height of screen
+        currentWidth=dm.widthPixels;
+        currentHeight= dm.heightPixels;
+
+        clickablePoints = new ArrayList();
+
+        clickablePoints.add(0,new Point(1745,1145));
+        clickablePoints.add(1,new Point(1850,1020));
+
+        this.setOnTouchListener(this);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(MapBitmap,0,0,paint); //Draw grass path background
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        switch(event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                int x = (int)event.getX();
+                int y = (int)event.getY();
+
+                Log.i("Point: ",x+"  "+y);
+                for(int i=0; i<clickablePoints.size();i++){
+                    if(x>clickablePoints.get(i).x*1600/3200.0-20
+                       &&x<clickablePoints.get(i).x*1600/3200+20
+                       &&y>clickablePoints.get(i).y-20
+                       &&y<clickablePoints.get(i).y+20){
+                        if(first)
+                            selected[0] = i;
+                        else
+                            selected[1] = i;
+
+
+                        Log.i("Index: ",i+" ");
+                    }
+                }
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                return true;
+            case MotionEvent.ACTION_UP:
+                return true;
+        }
+
+        return false;
+    }
+}
